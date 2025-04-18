8620867430860c45ecd59f9367821c082b5c4a25
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2025-04-18 11:10:17 -0400

    Flip animation

--- Game/scripts/card.gd
+++ Game/scripts/card.gd
@@ -1,22 +1,33 @@
-extends Control
+extends Button
 
 class_name CardBase
 
 @onready var card_spr: TextureRect = $button/card
 @onready var card: CardBase = $"."
+@onready var animations: AnimationPlayer = $Animations
 
 @export var ingredient_data: Ingredient
 
 signal card_selected(card)
 
+var is_face_down: bool
+
 func _ready() -> void:
-	emit_signal("card_selected", self)
+	card_selected.emit(self)
 	card_spr.texture = ingredient_data.card_sprite
+	is_face_down = true
+	set_to_face_down()
 
 func play_card():
 	pass
 
+func _on_pressed() -> void:
+	card_selected.emit(self)
 
+func set_to_face_down() -> void:
+	pass
 
-func _on_button_pressed() -> void:
-	emit_signal("card_selected", self)
+func set_to_face_up() -> void:
+	if is_face_down:
+		animations.play("flip_face_up")
+	is_face_down = false
