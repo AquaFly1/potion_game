ac131787601808682a4b4cc970dae6b8257b5231
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2025-04-18 12:01:07 -0400

    Hourra

--- Game/scenes/hand_manager.gd
+++ Game/scenes/hand_manager.gd
@@ -1,5 +1,17 @@
 class_name HandManager
 extends Control
 
+@onready var arc: Path2D = $Path2D
+
 func add_card_to_hand(card: CardBase) -> void:
-	pass
+	var path_follow: PathFollow2D = PathFollow2D.new()
+	arc.add_child(path_follow)
+	path_follow.add_child(card)
+	path_follow.v_offset = -100
+	update_arc()
+
+func update_arc() -> void:
+	var card_count: int = arc.get_child_count()
+	var gap_ratio: float = 1.0 / float(card_count)
+	for i in range(card_count):
+		arc.get_child(i).progress_ratio = gap_ratio * i
